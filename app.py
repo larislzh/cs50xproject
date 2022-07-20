f79fa8209e245db65b24cf42e2582d091f42543e
Laris Lee <larislzh@gmail.com>
2022-07-20 19:51:59 +0800

    First run

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+import datetime
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
