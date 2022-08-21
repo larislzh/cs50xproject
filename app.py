43a152da88f1d7bb3ef0dbce3a9ee210230ff32c
Laris Lee <larislzh@gmail.com>
2022-08-21 23:14:28 +0800

    Updating to sync with another device

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("index.html")
